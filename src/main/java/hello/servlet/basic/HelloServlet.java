2c18677a33b6e61a4d6d9a1b190e8536f47fa134
김동률 <dore@gimdonglyul-ui-MacBookAir.local>
2022-06-17 23:21:09 +0900

    git init

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,27 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="helloServlet", urlPatterns = "/hello")
+public class HelloServlet  extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 서블릿이 호출되면 service 메서드가 호출
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello" + username);
+        
+    }
+}
---
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,51 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet(name="requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        printStartLine(request);
+        printHeaders(request);
+    }
+
+    private void printHeaders(HttpServletRequest request) {
+        System.out.println("--- Headers - start ---");
+
+        /*
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while(headerNames.hasMoreElements()){
+            String headerName = headerNames.nextElement();
+            System.out.println("headerName = " + request.getHeader(headerName));
+        }
+         */
+        // 요즘 스타일
+        request.getHeaderNames().asIterator()
+                        .forEachRemaining(headerName -> System.out.println("headerName = " + headerName));
+
+
+        System.out.println("--- Headers - end ---");
+        System.out.println();
+    }
+
+    private void printStartLine(HttpServletRequest request) {
+        System.out.println("--- REQUEST-LINE start ---");
+        System.out.println("request.getMethod() = " + request.getMethod());
+        System.out.println("request.getProtocol() = " + request.getProtocol());
+        System.out.println("request.getScheme() = " + request.getScheme());
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+        System.out.println("request.getQueryString() = " + request.getQueryString());
+        System.out.println("request.isSecure() = " + request.isSecure());
+        System.out.println("--- REQUEST-LINE end ---");
+        System.out.println();
+
+    }
+}
